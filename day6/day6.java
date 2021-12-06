ed450ce9fd548ea319c0bb250e620e136bc91718
Amber Lee <amber_Lee1993@hotmail.com>
2021-12-05 23:23:04 -0800

    Day 6 part 1

---
+++ day6/day6.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Day6 {
+    public static void main(String[] args) {
+        List<Integer> input = new ArrayList<>(List.of(3,4,3,1,2));
+        List<Integer> appendices = new ArrayList<>();
+        int days = 80;
+        while (days > 0) {
+            int len = input.size();
+            for (int i = 0; i < len; i++) {
+                int age = input.get(i);
+                if (age == 0) {
+                    input.set(i, 6);
+                    appendices.add(8);
+                } else {
+                    input.set(i, age - 1);
+                }
+                if (appendices.size() > 0) {
+                    input.addAll(appendices);
+                }
+                appendices = new ArrayList<>();
+            }
+            days--;
+        }
+        System.out.println(input.size());
+    }
+}
