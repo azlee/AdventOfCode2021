122aab2030338cdf3acd2c7c6395ffa490aa0cb9
Amber Lee <amber_Lee1993@hotmail.com>
2021-12-09 21:14:19 -0800

    Day 10 part 1

---
+++ day10/day10.java
@@ -0,0 +1,70 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Stack;
+
+class Day10 {
+
+  static boolean isOpen(char c) {
+    return (c == '(' || c == '[' || c == '{' || c == '<');
+  }
+
+  static boolean isClosed(char c) {
+    return (c == ')' || c == ']' || c == '}' || c == '>');
+  }
+
+  static char getClosing(char c) {
+    if (c == '(') {
+      return ')';
+    }
+    if (c == '[') {
+      return ']';
+    }
+    if (c == '{') {
+      return '}';
+    }
+    if (c == '<') {
+      return '>';
+    }
+    return '\0';
+  }
+
+  static int getScore(char illegal) {
+    if (illegal == ')') {
+      return 3;
+    } else if (illegal == ']') {
+      return 57;
+    } else if (illegal == '}') {
+      return 1197;
+    } else if (illegal == '>') {
+      return 25137;
+    }
+    return 0;
+  }
+  public static void main(String[] args) {
+    try {
+      Path path = Paths.get("input.txt");
+      List<String> fileContents = Files.readAllLines(path);
+      Stack<Character> openBrackets = new Stack<Character>();
+      int syntaxErrorScore = 0;
+      for (String line : fileContents) {
+        char[] charArr = line.toCharArray();
+        for (char c : charArr) {
+          if (isOpen(c)) {
+            openBrackets.push(c);
+          } else if (isClosed(c)) {
+            char open = openBrackets.pop();
+            if (getClosing(open) != c) {
+              syntaxErrorScore += getScore(c);
+              break;
+            }
+          }
+        }
+      }
+      System.out.println(syntaxErrorScore);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+}
